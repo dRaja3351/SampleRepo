1b4a15a17416f2e38d29da3235c118f28e1bb420
draja3351 <draja2833@gmail.com>
2023-10-28 10:24:14 +0530

    Commiting new project into git
    Signed-off-by: draja3351 <draja2833@gmail.com>

---
+++ projectFromGIT/src/projectFromGIT/CommitinGIT.java
@@ -0,0 +1,11 @@
+package projectFromGIT;
+
+public class CommitinGIT {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to CI process");
+		System.out.println("Commiting the code from Eclipse");
+
+	}
+
+}
